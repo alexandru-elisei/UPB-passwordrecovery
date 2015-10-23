c083318e67228fdf43a97507d8d439fa5f1cb170
Alexandru Elisei <alexandru.elisei@gmail.com>
2015-10-23 09:13:42 +0300

    Reset authinput field and button on page load/refresh

--- password_recovery.js
+++ password_recovery.js
@@ -1,41 +1,45 @@
-window.onload = initInputAuth;
+window.onload = disableInputAuth;
 
-function initInputAuth() {
-	document.getElementById("inputauth").value = "";
-	document.getElementById("resetbutton").disabled = true;
+function disableInputAuth() {
+	document.getElementById("authselect").value="DefaultMessage";
+	document.getElementById("authbuttonreset").disabled = true;
+	document.getElementById("authinput").value = "";
+	document.getElementById("authinput").setAttribute("style", "");
+	document.getElementById("authinput").disabled = true;
 }
 
 function enableInputAuth() {
-	if (document.getElementById("selectauth").value == "Email") {
-		document.getElementById("inputauth").disabled = false;
-		document.getElementById("inputauth").value = "Email";
-		document.getElementById("inputauth").setAttribute("style", "font-style: italic; color: grey");
-		document.getElementById("resetbutton").disabled = true;
-	} else if (document.getElementById("selectauth").value == "UserId") {
-		document.getElementById("inputauth").disabled = false;
-		document.getElementById("inputauth").value = "UserId";
-		document.getElementById("inputauth").setAttribute("style", "font-style: italic; color: grey");
-		document.getElementById("resetbutton").disabled = true;
-	} else if (document.getElementById("selectauth").value == "NrTel") {
-		document.getElementById("inputauth").disabled = false;
-		document.getElementById("inputauth").value = "Nr. Tel.";
-		document.getElementById("inputauth").setAttribute("style", "font-style: italic; color: grey");
-		document.getElementById("resetbutton").disabled = true;
+	if (document.getElementById("authselect").value == "Email") {
+		document.getElementById("authinput").disabled = false;
+		document.getElementById("authinput").value = "Email";
+		document.getElementById("authinput").setAttribute("style", "font-style: italic; color: grey");
+		document.getElementById("authbuttonreset").disabled = true;
+	} else if (document.getElementById("authselect").value == "UserId") {
+		document.getElementById("authinput").disabled = false;
+		document.getElementById("authinput").value = "UserId";
+		document.getElementById("authinput").setAttribute("style", "font-style: italic; color: grey");
+		document.getElementById("authbuttonreset").disabled = true;
+	} else if (document.getElementById("authselect").value == "NrTel") {
+		document.getElementById("authinput").disabled = false;
+		document.getElementById("authinput").value = "Nr. Tel.";
+		document.getElementById("authinput").setAttribute("style", "font-style: italic; color: grey");
+		document.getElementById("authbuttonreset").disabled = true;
 	} else {
-		document.getElementById("resetbutton").disabled = true;
-		document.getElementById("inputauth").disabled = true;
+		document.getElementById("authbuttonreset").disabled = true;
+		document.getElementById("authinput").value = "";
+		document.getElementById("authinput").disabled = true;
 	}
 }
 
 function prepareInputAuth() {
-	if (document.getElementById("inputauth").disabled == false &&
-			document.getElementById("inputauth").style.color == "grey") {
-		document.getElementById("inputauth").value = "";
-		document.getElementById("inputauth").setAttribute("style", "");
-		document.getElementById("resetbutton").disabled = true;
+	if (document.getElementById("authinput").disabled == false &&
+			document.getElementById("authinput").style.color == "grey") {
+		document.getElementById("authinput").value = "";
+		document.getElementById("authinput").setAttribute("style", "");
+		document.getElementById("authbuttonreset").disabled = true;
 	}
 }
 
 function enableResetButton() {
-	document.getElementById("resetbutton").disabled = false;
+	document.getElementById("authbuttonreset").disabled = false;
 }
