1ca2d5697f71928c0402cc5b6eca13a12c68951f
Alexandru Elisei <alexandru.elisei@gmail.com>
2015-10-25 18:24:40 +0200

    Add captcha validation

--- password_recovery.js
+++ password_recovery.js
@@ -1,11 +1,41 @@
 window.onload = disableInputAuth;
 
 function disableInputAuth() {
+	/*
+	var getRequestParam = window.location.search.substring(1);
+	var paramArray = getRequestParam.split('&');
+	var paramCount = paramArray.length;
+	var isFailedCaptcha = true;
+
+	if (paramCount != 2) {
+		isFailedCaptcha = false;
+	} else {
+		for (var i = 0; i < paramCount; i++) {
+			var keyValue = paramArray[i].split('=');
+			// First get parameter is always authmethod
+			if (i == 0) { 
+				if (keyValue[0] != "authmethod") {
+					isFailedCaptcha = false;
+					break;
+				} else if (keyValue[1] != "email" && keyValue[1] != "userid" && keyValue[1] != "telnum") {
+					isFailedCaptcha = false;
+					break;
+				}
+			} else if (keyValue[0] != "authdata") {
+				isFailedCaptcha = false;
+				break;
+			}
+		}
+	}
+	*/
+
 	document.getElementById("authmethod").value="DefaultMessage";
 	document.getElementById("authbuttonreset").disabled = true;
 	document.getElementById("authdata").value = "";
+	document.getElementById("authdata").value = getRequestParams;
 	document.getElementById("authdata").setAttribute("style", "");
 	document.getElementById("authdata").disabled = true;
+	document.getElementById("authbuttonreset").disabled = true;
 }
 
 function enableInputAuth() {
@@ -43,3 +73,36 @@ function prepareInputAuth() {
 function enableResetButton() {
 	document.getElementById("authbuttonreset").disabled = false;
 }
+
+// Browser Support Code
+function ajaxFunction(){
+	var ajaxRequest;
+
+	document.getElementById("authdata").value = "huh";
+	try {
+		// Opera 8.0+, Firefox, Safari
+		ajaxRequest = new XMLHttpRequest();
+	} catch (e) {
+		// Internet Explorer Browsers
+		try{
+			ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e) {
+			try { 
+				ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (e) {
+				// Something went wrong
+				alert("Your browser broke!");
+				return false; 
+			}
+		}
+	}
+
+	ajaxRequest.onreadystatechange = function() {
+		if (ajaxRequest.readyState == 4) {
+			document.getElementById("authdata").value = "huh";
+		}
+		ajaxRequest.open("GET", "validate_captcha.php", true);
+		ajaxRequest.send(null);
+	}
+}
+
--- password_recovery.php
+++ password_recovery.php
@@ -1,38 +1,35 @@
+<?php
+$sitekey = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
+$authmethods = array("email", "userid", "telnum");
+?>
+
 <html>
 <head>
 	<title>Password Recovery</title>
 	<link rel="stylesheet" type="text/css" href="password_recovery.css" />
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<script src="password_recovery.js"></script>
-	<script type="text/javascript">
-		var onloadCallback = function() {
-			grecaptcha.render("recaptcha", {"sitekey": "a", "theme": "light"});
-		}
-	</script>
-	<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer>
 	</script>
 </head>
-<body id="body">
 
+<body id="body">
 	<div id="header">
 		<a id="logo" title="Universitatea Politehnica Bucuresti" href="http://www.upb.ro/"></a>
 	</div>
 
 	<div id="content">
-
 		<p class="info">Vă rugăm să alegeți metoda preferată de resetare a parolei (UserID, Email personal sau Nr. tel. mobil), apoi completați câmpul de mai jos. Toate trei provin de pe platforma <a href="http://studenti.pub.ro">studenti.pub.ro</a>:
-        <ul>
-          <li><b>UserID</b>: poate fi găsit în pagina <b> Acces site cursuri </b></li>
-          <li><b>Email personal</b>: poate fi găsit în pagina <b> Date personale </b>, câmpul <i>E-mail</i></li>
-          <li><b>Nr. tel. mobil</b>: poate fi găsit în pagina <b> Date personale </b>, câmpul <i>Telefon mobil</i></li>
-        </ul>
-        </p>
-        <p class="info">Un cod de verificare va fi trimis către adresa de e-mail personală cu instrucțiuni de resetare a parolei.</p>
+		<ul>
+		  <li><b>UserID</b>: poate fi găsit în pagina <b> Acces site cursuri </b></li>
+		  <li><b>Email personal</b>: poate fi găsit în pagina <b> Date personale </b>, câmpul <i>E-mail</i></li>
+		  <li><b>Nr. tel. mobil</b>: poate fi găsit în pagina <b> Date personale </b>, câmpul <i>Telefon mobil</i></li>
+		</ul>
+		</p>
+		<p class="info">Un cod de verificare va fi trimis către adresa de e-mail personală cu instrucțiuni de resetare a parolei.</p>
 
-		<form action='forgot-passwordck.php' method=post>
+		<form action="validate_captcha.php" method=post>
 			<div id="authsection">
 				</br>
-				<p class="authinfo">
 				<select id="authmethod" name="authmethod" onchange="enableInputAuth()">
 					<option selected disabled value="DefaultMessage">Selectați metoda de autentificare</option>
 					<option value="userid">UserID</option>
@@ -42,8 +39,10 @@
 				<p class="authinfo">Introduceți datele de autentificare:</p>
 				<input id="authdata" name="authdata" size="25" type="text" disabled onclick="prepareInputAuth()" oninput="enableResetButton()"/>
 				</br>
-				<p class="authinfo">Introduceți codul de verificare:</p>
-				<div id="recaptcha"></div>
+				<p class="authinfo">Sunteți un robot?</p>
+				<div id="recaptcha" class="g-recaptcha" data-sitekey=<?php echo $sitekey; ?>> </div>
+<script type="text/javascript" src="https://www.google.com/recaptcha/api.js?hl=ro" async defer>
+</script>
 				</br>
 				<input id="authbuttonreset" type="submit" disabled value="Reset"/>
 			</div>
