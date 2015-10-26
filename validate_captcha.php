6949200772b89977a49722f4e213c64d05226166
Alexandru Elisei <alexandru.elisei@gmail.com>
2015-10-26 18:06:50 +0200

    Sanitize captcha validation request

--- validate_captcha.php
+++ validate_captcha.php
@@ -3,9 +3,11 @@ require ("recaptcha/src/autoload.php");
 
 $secret = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
 $recaptcha = new \ReCaptcha\ReCaptcha($secret);
-$resp = $recaptcha->verify($_POST["g-recaptcha-response"]);
 
-if ($resp->isSuccess()) {
+$response = filter_var(trim($_POST["g-recaptcha-response"]), FILTER_SANITIZE_STRING);
+$validation = $recaptcha->verify($response);
+
+if ($validation->isSuccess()) {
 	echo "success";
 } else {
 	echo "failure";
