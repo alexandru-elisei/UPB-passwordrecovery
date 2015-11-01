ad58b60d2372db8367e1d1112bb6292418cf5304
Alexandru Elisei <alexandru.elisei@gmail.com>
2015-11-01 18:30:59 +0200

    Use bootstrap and replace javascript with jQuery

--- password_recovery.js
+++ password_recovery.js
@@ -1,107 +1,39 @@
-window.onload = disableInputAuth;
+$(document).ready(function() {
+	$("#authdata").prop("placeholder", $("#authmethod option:selected").text());
 
-function disableInputAuth() {
-	document.getElementById("authmethod").value="DefaultMessage";
-	document.getElementById("authbuttonreset").disabled = true;
-	document.getElementById("authdata").value = "";
-	document.getElementById("authdata").setAttribute("style", "");
-	document.getElementById("authdata").disabled = true;
-	document.getElementById("authbuttonreset").disabled = true;
-}
+	$("#authmethod").change(function() {
+		$("#authdata").prop("placeholder", $("#authmethod option:selected").text());
+	});
 
-function enableInputAuth() {
-	if (document.getElementById("authmethod").value == "email") {
-		document.getElementById("authdata").disabled = false;
-		document.getElementById("authdata").value = "Email";
-		document.getElementById("authdata").setAttribute("style", "font-style: italic; color: grey");
-		document.getElementById("authbuttonreset").disabled = true;
-	} else if (document.getElementById("authmethod").value == "userid") {
-		document.getElementById("authdata").disabled = false;
-		document.getElementById("authdata").value = "UserID";
-		document.getElementById("authdata").setAttribute("style", "font-style: italic; color: grey");
-		document.getElementById("authbuttonreset").disabled = true;
-	} else if (document.getElementById("authmethod").value == "telnum") {
-		document.getElementById("authdata").disabled = false;
-		document.getElementById("authdata").value = "Nr. Tel.";
-		document.getElementById("authdata").setAttribute("style", "font-style: italic; color: grey");
-		document.getElementById("authbuttonreset").disabled = true;
-	} else {
-		document.getElementById("authbuttonreset").disabled = true;
-		document.getElementById("authdata").value = "";
-		document.getElementById("authdata").disabled = true;
-	}
-}
-
-function prepareInputAuth() {
-	if (document.getElementById("authdata").disabled == false &&
-			document.getElementById("authdata").style.color == "grey") {
-		document.getElementById("authdata").value = "";
-		document.getElementById("authdata").setAttribute("style", "");
-		document.getElementById("authbuttonreset").disabled = true;
-	}
-}
-
-function enableResetButton() {
-	document.getElementById("authbuttonreset").disabled = false;
-}
-
-var validationRequest;
-
-// Browser Support Code
-function validateCaptcha(){
-
-	try {
-		// Opera 8.0+, Firefox, Safari
-		validationRequest = new XMLHttpRequest();
-	} catch (e) {
-		// Internet Explorer Browsers
-		try{
-			validationRequest = new ActiveXObject("Msxml2.XMLHTTP");
-		} catch (e) {
-			try { 
-				validationRequest = new ActiveXObject("Microsoft.XMLHTTP");
-			} catch (e) {
-				// Something went wrong
-				alert("Your browser broke!");
-			}
-		}
-	}
-	if (validationRequest != null) {
-		var response = grecaptcha.getResponse();
-		validationRequest.open("POST", "validate_captcha.php", true);
-		validationRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		validationRequest.send("g-recaptcha-response=" + response);
-		validationRequest.onreadystatechange = verifyValidateCaptcha;
-	}
-}
-
-function verifyValidateCaptcha() {
-	if (validationRequest.readyState == 4) {
-		if (validationRequest.responseText == "success") {
-			// Creating phony form for submitting POST data
-			var phonyForm = document.createElement("form");
-			phonyForm.setAttribute("method", "post");
-			phonyForm.setAttribute("action", "form_submit_success.php");
-
-			// Phony authmethod input field
-			var authMethod = document.createElement("input");
-			authMethod.setAttribute("type", "hidden");
-			authMethod.setAttribute("name", "authmethod");
-			authMethod.setAttribute("value", document.getElementById("authmethod").value);
-			phonyForm.appendChild(authMethod);
-
-			// Phony authdata input field
-			var authData = document.createElement("input");
-			authData.setAttribute("type", "hidden");
-			authData.setAttribute("name", "authdata");
-			authData.setAttribute("value", document.getElementById("authdata").value);
-			phonyForm.appendChild(authData);
-
-			document.body.appendChild(phonyForm);
-			phonyForm.submit();
+	$("#authdata").keyup(function() {
+		if ($("#authdata").val().length == 0) {
+			$("#authbuttonreset").prop("disabled", true);
 		} else {
-			document.getElementById("captchainfo").childNodes[0].nodeValue = "Verificare eșuată. Sunteți un robot?";
-			document.getElementById("captchainfo").setAttribute("style", "color: red; font: Helvetica; font-size: 15;");
-		}
-	}
-}
+			$("#authbuttonreset").prop("disabled", false);
+		}	
+	});
+
+	$("#authform").submit(function(event) {
+		event.preventDefault();
+
+		var captchaResponse = grecaptcha.getResponse();
+		$.post("validate_captcha.php", {"g-recaptcha-response": captchaResponse}, function(data) {
+			if (data === "success") {
+				// Creating phony form to redirect with post data
+				var url = "forgot-passwordck.php";
+				var authmethod = $("#authmethod option:selected").text().toLowerCase();
+				var authdata = $("#authdata").val();
+				var form = $('<form action="' + url + '" method="post">' +
+						'<input type="text" name="authmethod" id="authmethod" value="' + authmethod + '">' + 
+						'<input type="text" name="authdata" id="authdata" value="' + authdata + '">' + 
+						'</form>');
+				$("body").append(form);
+				form.submit();
+			} else {
+				$("#captchainfo").text("Verificare eșuată. Sunteți un robot?");
+				$("#captchainfo").css("color", "red");
+				$("#captchainfo").css("font-weight", "bold");
+			}
+		});
+	});
+});
--- password_recovery.php
+++ password_recovery.php
@@ -1,84 +1,72 @@
 <?php
 require("config.php");
 ?>
-
 <!DOCTYPE html>
-<html lang="ro">
+<html lang="en">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<script src="password_recovery.js"></script>
 	<link rel="stylesheet" type="text/css" href="password_recovery.css">
-	<script type="text/javascript" src="password_recovery.js"></script>
 	<title>Password Recovery</title>
 </head>
 
-<body class="container">
-	<div id="header">
-		<a id="logo" title="Universitatea Politehnica Bucuresti" href="http://www.upb.ro/">
-			<img src="images/logo.gif" alt="UPB logo" class="img" width="580" height="50">
-		</a>
+<body>
+	<div class="page-header">
+		<img class="img-responsive center-block" alt="UPB logo" src="images/logo.gif" width="580" height="50">
 	</div>
+	<div class="container">
+		<div class="text-justify">
+			<br/>
+			<p class="info">Vă rugăm să alegeți metoda preferată de resetare a parolei (UserID, Email personal sau Nr. tel. mobil), apoi completați câmpul de mai jos. Toate trei provin de pe platforma <a href="http://studenti.pub.ro">studenti.pub.ro</a>:
+			<ul class="list">
+				<li><strong>UserID</strong>: poate fi găsit în pagina <strong>Acces site cursuri</strong></li>
+				<li><strong>Email personal</strong>: poate fi găsit în pagina <strong>Date personale</strong>, câmpul <i>E-mail</i></li>
+				<li><strong>Nr. tel. mobil</strong>: poate fi găsit în pagina <strong>Date personale</strong>, câmpul <i>Telefon mobil</i></li>
+			</ul>
+			<p class="info">Un cod de verificare va fi trimis către adresa de e-mail personală cu instrucțiuni de resetare a parolei.</p>
+		</div>
+		<br />
 
-	<div id="content" class="text-justify">
-		<p class="info">Vă rugăm să alegeți metoda preferată de resetare a parolei (UserID, Email personal sau Nr. tel. mobil), apoi completați câmpul de mai jos. Toate trei provin de pe platforma <a href="http://studenti.pub.ro">studenti.pub.ro</a>:
-		<ul class="list">
-		  <li><strong>UserID</strong>: poate fi găsit în pagina <strong>Acces site cursuri</strong></li>
-		  <li><strong>Email personal</strong>: poate fi găsit în pagina <strong>Date personale</strong>, câmpul <i>E-mail</i></li>
-		  <li><strong>Nr. tel. mobil</strong>: poate fi găsit în pagina <strong>Date personale</strong>, câmpul <i>Telefon mobil</i></li>
-		</ul>
-		<p class="info">Un cod de verificare va fi trimis către adresa de e-mail personală cu instrucțiuni de resetare a parolei.</p>
-
-		<form role="form" action="javascript:validateCaptcha()" method=post>
-			<div class="form-group row">
-				<div class="col-sm-6">
-					<label for"authmethod">Metoda de autentificare:</label>
-					<br/>
-					<select class="form-control" id="authmethod" name="authmethod" onchange="enableInputAuth()">
-						<option selected value="userid">UserID</option>
-						<option value="email">Email</option>
-						<option disabled value="telnum">Nr. tel. mobil (indisponibil)</option>
-					</select>
-				</div>
+		<form id="authform" role="form">
+			<div class="form-group">
+				<label for="authmethod">Metoda de autentificare:</label>
+				<br/>
+				<select class="form-control" id="authmethod" name="authmethod">
+					<option selected value="userid">UserID</option>
+					<option value="email">Email</option>
+					<option disabled value="telnum">Nr. tel. mobil (indisponibil)</option>
+				</select>
 			</div>
-			<div class="form-group row">
-				<div class="col-sm-6">
-					<label for="authdata">Datele de autentificare:</label>
-					<input class="form-control" id="authdata" name="authdata" size="25" type="text" disabled onclick="prepareInputAuth()" oninput="enableResetButton()"/>
-				</div>
+			<div class="form-group">
+				<label for="authdata">Datele de autentificare:</label>
+				<input class="form-control" id="authdata" name="authdata" type="text">
 			</div>
-			<div class="form-group row">
-				<div class="col-sm-8">
-					<p class="authinfo" id="captchainfo"><strong>Sunteți un robot?</strong></p>
-					<div id="recaptcha" class="g-recaptcha" data-sitekey=<?php echo $captchasitekey; ?>> </div>
-					<script type="text/javascript" src="https://www.google.com/recaptcha/api.js?hl=ro" async defer>
-					</script>
-				</div>
+			<div class="form-group">
+				<p class="authinfo" id="captchainfo"><strong>Sunteți un robot?</strong></p>
+				<div id="recaptcha" class="g-recaptcha" data-sitekey=<?php echo $captchasitekey; ?>> </div>
+				<script type="text/javascript" src="https://www.google.com/recaptcha/api.js?hl=ro" async defer>
+				</script>
 			</div>
 			<div class="form-group">
 				<input class="btn btn-default" id="authbuttonreset" type="submit" disabled value="Reset"/>
 			</div>
 		</form>
 
-		<div id="footer">
-			<img id="img_footer" alt="UPB footer" src='images/bg-footer.gif' />
-			<table border="0" id="bottom_links">
-				<tr>
-					<td class="link_cells" id="cell01">
-						<a class="btn btn-link bottom_links" role="button" href="http://studenti.pub.ro">studenti.pub.ro</a>
-					</td>
-					<td class="link_cells" id="cell02">
-						<a class="btn btn-link bottom_links" role="button" href="http://curs.pub.ro">curs.pub.ro</a>
-					</td>
-					<td class="link_cells" id="cell03">
-						<a class="btn btn-link bottom_links" role="button" href="http://www.upb.ro">upb.ro</a>
-					</td>
+		<div class="page-footer text-center">
+			<img id="footer-img" class="img-responsive center-block" alt="UPB footer" src='images/bg-footer.gif' width="630" height="50">
+			<table id="bottom-links">
+				<tr> 
+					<td id="cell1"><a class="btn-link" href="http://studenti.pub.ro">studenti.pub.ro</a></td>
+					<td id="cell2"><a class="btn-link" href="http://curs.pub.ro">curs.pub.ro</a></td>
+					<td id="cell3"><a class="btn-link" href="http://www.upb.ro">upb.ro</a></td>
 				</tr>
 			</table>
 		</div>
-	</div>
 
+	</div>
 </body>
 </html>
